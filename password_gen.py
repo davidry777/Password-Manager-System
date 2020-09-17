7c36d84d928311a9cab71af243d2f82165bbd621
David Ryan <dryan0319@gmail.com>
2020-09-17 11:27:38 -0700

    Created Username and Website class with getter and setter methods

--- password_gen.py
+++ password_gen.py
@@ -22,3 +22,43 @@ class Password:
 
     def set_password(self, new_password):
         self.password = new_password
+
+
+class Username:
+    """Class that represents the websites account(the Username)
+
+    Attributes:
+        username (str): The username for this account
+        website_name (str): The name of the website the username belongs to
+
+    Methods:
+        get_username: Used to return the password
+        set_username: Used to set a new password to the user's account
+    """
+
+    def __init__(self, username, website_name):
+        self.username = username
+        self.website_name = website_name
+
+    def get_username(self):
+        return self.username
+
+    def set_username(self, new_username):
+        self.username = new_username
+
+
+class Website:
+    """Class that represents the websites account(the Username)
+
+    Attributes:
+        website_name (str): The name of the website for all accounts under it
+
+    Methods:
+        get_website: Used to return the name of the website
+    """
+
+    def __init__(self, website_name):
+        self.website_name = website_name
+
+    def get_website(self):
+        return self.username
