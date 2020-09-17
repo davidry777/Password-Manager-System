d17181f15e2495b7a4b7b5630fb069b90d813e1f
David Ryan <dryan0319@gmail.com>
2020-09-16 17:09:36 -0700

    created the Password class in password_gen.py

---
+++ password_gen.py
@@ -0,0 +1,10 @@
+from password_generator import PasswordGenerator
+
+
+class Password:
+    """Class to represent a password
+
+    Attributes:
+        password (str): The password for the username
+        username (str): The username that owns this password
+    """
