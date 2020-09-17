db6df167db7e8da38ce5f45bd4a12bfe489d6d06
David Ryan <dryan0319@gmail.com>
2020-09-16 17:28:11 -0700

    Created new methods, get_password and set_password, in Password class

--- password_gen.py
+++ password_gen.py
@@ -7,4 +7,18 @@ class Password:
     Attributes:
         password (str): The password for the username
         username (str): The username that owns this password
+
+    Methods:
+        get_password: Used to return the password
+        set_password: Used to set a new password to the user's account
     """
+
+    def __init__(self, password, username):
+        self.password = password
+        self.username = username
+
+    def get_password(self):
+        return self.password
+
+    def set_password(self, new_password):
+        self.password = new_password
