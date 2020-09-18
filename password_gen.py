418b66dd8ab003287a7d83fec6839f8b4d4ec8de
David Ryan <dryan0319@gmail.com>
2020-09-18 09:55:31 -0700

    Created the find_object method which checks to see if the website is already added in the list

--- password_gen.py
+++ password_gen.py
@@ -64,6 +64,13 @@ class Website:
         account = Username(username)
         self.usernames.append(account)
 
+def find_object(field, object_list):
+    """Check 'object_list' to see if an object with a 'name' attribute equal to 'field' exists, return it if so."""
+    for item in object_list:
+        if item.name == field:
+            return item
+    return None
+
 
 def generate_list():
     with open("default_accountList.txt", "r") as passwords:
