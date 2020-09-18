b76919832d7abc8f4c98032d7b984dacfcada43f
David Ryan <dryan0319@gmail.com>
2020-09-18 11:31:51 -0700

    Made the Creator class which handles user input and password generator

--- password_creator.py
+++ password_creator.py
@@ -1,1 +1,14 @@
 from password_gen import Website, Username, Password
+from password_generator import PasswordGenerator
+
+
+class Creator:
+    """Class for creating a brand new password
+
+    Static Methods:
+        website_input (str): Receives user input for what website their new account belongs to.
+        username_input (str): Receives user input for the username of their new account
+        password_input (str): Receives user input for the password of their new account
+        generate_password (str): Creates a randomly generated password using the PasswordGenerator library
+    """
+
