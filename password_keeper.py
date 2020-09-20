65f76c6983d88c72618d1091bfe851793121a309
David Ryan <dryan0319@gmail.com>
2020-09-19 19:51:15 -0700

    Added more menu options

--- password_keeper.py
+++ password_keeper.py
@@ -2,4 +2,8 @@ def print_menu():
     print("-----------------------------")
     print("[1] Create a new password")
     print("[2] Print password list")
+    print("[3] Search for password")
+    print("[4] Change a password")
+    print("[5] Change username")
+    print("[0] Quit")
     print("-----------------------------")
